e9be0fdfb113cf1804a375c62df60a3a5dd31fd4
Aeuke Peer Conradi <peerconradi@gmail.com>
2020-12-27 09:28:49 +0100

    First implementations

---
+++ src/MUNitySchema/Schema/Authentication/RegisterRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MUNitySchema.Schema.Authentication
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [MaxLength(40)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Forename { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Lastname { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string Mail { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+    }
+}
