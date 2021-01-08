0871f3e754f71b6c024477fd723823a16b79b325
Aeuke Peer Conradi <peerconradi@gmail.com>
2021-01-08 17:58:36 +0100

    made abstractionLayer for SimulationUser

--- MUNitySchema/Schema/Simulation/IUserItem.cs
+++ MUNitySchema/Schema/Simulation/IUserItem.cs
@@ -5,39 +5,28 @@ using System.Text;
 namespace MUNity.Schema.Simulation
 {
     /// <summary>
-    /// User Package for the administrators that can see and edit the users.
+    /// The IUserItem Interface is for any kind of SimulationUser Implementation
     /// </summary>
-    public class SimulationUserSetup : IUserItem
+    public interface IUserItem
     {
         /// <summary>
         /// The simulation user Id.
         /// </summary>
-        public int SimulationUserId { get; set; }
+        int SimulationUserId { get; set; }
 
         /// <summary>
         /// The display name of the user.
         /// </summary>
-        public string DisplayName { get; set; }
+        string DisplayName { get; set; }
 
         /// <summary>
         /// The current role this user has picked.
         /// </summary>
-        public int RoleId { get; set; }
+        int RoleId { get; set; }
 
         /// <summary>
         /// Returns of the user is connected or not. This is known if there is an active Hub Connection.
         /// </summary>
-        public bool IsOnline { get; set; }
-
-
-        /// <summary>
-        /// The Public Id of the user.
-        /// </summary>
-        public string PublicId { get; set; }
-
-        /// <summary>
-        /// The password of the user.
-        /// </summary>
-        public string Password { get; set; }
+        bool IsOnline { get; set; }
     }
 }
--- MUNitySchema/Schema/Simulation/SimulationUserItem.cs
+++ MUNitySchema/Schema/Simulation/SimulationUserItem.cs
@@ -7,7 +7,7 @@ namespace MUNity.Schema.Simulation
     /// <summary>
     /// The user of a simulation.
     /// </summary>
-    public class SimulationUserItem
+    public class SimulationUserItem : IUserItem
     {
         /// <summary>
         /// The simulation user Id.
