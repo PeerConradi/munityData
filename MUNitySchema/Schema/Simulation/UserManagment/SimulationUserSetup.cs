8a95d0fda4591a82bc8a4a931da455cdf4fce3b3
Aeuke Peer Conradi <peerconradi@gmail.com>
2021-02-02 20:58:41 +0100

    New Petition Structure and Version 1.0.32

--- MUNitySchema/Schema/Simulation/UserManagment/SimulationUserSetup.cs
+++ MUNitySchema/Schema/Simulation/UserManagment/SimulationUserSetup.cs
@@ -7,28 +7,8 @@ namespace MUNity.Schema.Simulation
     /// <summary>
     /// User Package for the administrators that can see and edit the users.
     /// </summary>
-    public class SimulationUserSetup : IUserItem
+    public class SimulationUserSetup : SimulationUserItem
     {
-        /// <summary>
-        /// The simulation user Id.
-        /// </summary>
-        public int SimulationUserId { get; set; }
-
-        /// <summary>
-        /// The display name of the user.
-        /// </summary>
-        public string DisplayName { get; set; }
-
-        /// <summary>
-        /// The current role this user has picked.
-        /// </summary>
-        public int RoleId { get; set; }
-
-        /// <summary>
-        /// Returns of the user is connected or not. This is known if there is an active Hub Connection.
-        /// </summary>
-        public bool IsOnline { get; set; }
-
 
         /// <summary>
         /// The Public Id of the user.
